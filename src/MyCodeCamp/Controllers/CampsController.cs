ae41ce9ca43bbc44ef62e02c48d674c8b174845c
Roberto Navarro <navarrorc@users.noreply.github.com>
2017-01-23 23:17:34 -0600

    Initial Commit

---
+++ src/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCodeCamp.Controllers
+{
+    public class CampsController : Controller
+    {
+        [HttpGet("api/camps")]
+        public IActionResult Get()
+        {
+            return Ok(new {Name = "Roberto", FavoriteColor = "Red"});
+        }
+    }
+}
